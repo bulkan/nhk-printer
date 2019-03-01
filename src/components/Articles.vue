1aa488456b516fe2b19cdffedf1deb541881491b
Bulkan Evcimen <bulkan@gmail.com>
2019-03-01 16:54:54 +1100

    Adding NHKArticle

---
+++ src/components/Articles.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="articles">
+    <div class="sheet">
+      <div class="columns">
+        <div class="col">
+          <article></article>
+        </div>
+
+        <div class="col">
+          <article>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NHKArticle from "./NHKArticle";
+
+export default {
+  components: {
+    'nhk-article': NHKArticle
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+.articles {
+  .sheet {
+    margin: 0;
+  }
+
+  .columns {
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+
+    .col {
+      padding: 10px;
+      width: 30%;
+      min-height: 90vh;
+      border: 1px solid #cacaca;
+
+      &:first-child {
+        margin-right: 5%;
+      }
+    }
+  }
+
+  @media print {
+    .sheet {
+      margin: 5mm auto;
+    }
+
+    .columns {
+      .col {
+        min-height: auto;
+        height: 200mm;
+        width: 90mm;
+        border: 1px dashed #dfdfdf;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/NHKArticle.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nhkArticle">
+    <div>
+      <input placeholder="Article Id" />
+      <button>Scrape</button>
+    <div>
+
+    <article>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    articleId: String
+  },
+
+  mounted() {
+
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.nhkArticle {
+  article {
+    font-size: 18px;
+    padding: 10px;
+    width: 30%;
+    min-height: 90vh;
+    border: 1px solid #cacaca;
+  }
+}
+
+@media print {
+  .nhkArticle {
+    .article {
+      font-size: 10px;
+    }
+  }
+}
+</style>
