d03b1b65686f1e2bd4adfc5d30828202de9ff31c
Bulkan Evcimen <bulkan@gmail.com>
2019-05-07 21:45:44 +1000

    Allow text selection

--- src/components/NHKArticle.vue
+++ src/components/NHKArticle.vue
@@ -46,6 +46,10 @@ export default {
   article {
     font-size: 18px;
     padding: 10px;
+    -webkit-user-select: all;  /* Chrome all / Safari all */
+    -moz-user-select: all;     /* Firefox all */
+    -ms-user-select: all;      /* IE 10+ */
+    user-select: all;          /* Likely future */   
   }
 }
 
